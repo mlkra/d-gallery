52f803b07a1f28bc6e7f825339663659c5ac9041
Michał Krasoń <kramichal96@gmail.com>
2018-12-15 21:51:31 +0100

    Remove unused import

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { SignGuard } from './guard/sign.guard';
 
 const routes: Routes = [
   {
